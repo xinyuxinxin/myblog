06ddc7b145e68decc52ee33191ba2b89b44ae3dd
游亿增 <1570222321@qq.com>
2020-11-10 16:39:58 +0800

    create

---
+++ .vuepress/config.js
@@ -0,0 +1,119 @@
+module.exports = {
+  // 标题
+  "title": "心语新心",
+  // 描述
+  "description": "学习博客",
+  // 部署时输出文件夹
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  //挂载主题
+  "theme": "reco",
+  "themeConfig": {
+    // 导航栏
+    "nav": [
+      {
+        "text": "首页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间轴",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "信息",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "联系方式",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2020"
+  },
+  "markdown": {
+    "lineNumbers": true
+  },
+  plugins: [
+      [
+          'ribbon',
+        {
+          size:90,
+          opacity: 0.8,
+          zIndex: -1
+        }
+      ],
+    [
+        '@vuepress/back-to-top'
+    ]
+  ]
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+---
+home: true
+heroText: 心语新心
+tagline: 过好每一天，充实每一天
+# heroImage: /hero.png
+# heroImageStyle: {
+#   maxWidth: '600px',
+#   width: '100%',
+#   display: block,
+#   margin: '9rem auto 2rem',
+#   background: '#fff',
+#   borderRadius: '1rem',
+# }
+bgImage: http://conferencehuizhan.oss-cn-beijing.aliyuncs.com/20201027/5cf18a2504b74d2693824cc5e9ef0ee2.png
+bgImageStyle: {
+  height: '450px'
+}
+isShowTitleInHome: false
+actionText: Guide
+actionLink: /views/other/guide
+features:
+- title: Yesterday
+  details: 忘记昨天
+- title: Today
+  details: 享受今天
+- title: Tomorrow
+  details: 期待明天
+---
